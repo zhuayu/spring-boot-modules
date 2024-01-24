cbba7590aee764f90d80a1419d9b850256460004
Jax <294242589@qq.com>
2024-01-24 17:43:17 +0800

    refactor: clear code

--- user-service/src/main/java/com/aitschool/user/Rabc/repository/AdministratorRepository.java
+++ user-service/src/main/java/com/aitschool/user/Rabc/repository/AdministratorRepository.java
@@ -22,8 +22,6 @@ public interface AdministratorRepository extends PagingAndSortingRepository<Admi
 
     Administrator findById(Long administratorId);
 
-    Optional<Administrator> findByUserIdAndRolesId(Long userId, Long id);
-
     Optional<Administrator> findByUserId(Long userId);
 }
 
--- user-service/src/main/java/com/aitschool/user/Rabc/repository/PermissionRepository.java
+++ user-service/src/main/java/com/aitschool/user/Rabc/repository/PermissionRepository.java
@@ -3,13 +3,10 @@ package com.aitschool.user.Rabc.repository;
 import com.aitschool.user.Rabc.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
     Optional<Permission> findByName(String name);
 
-
-    List<Permission> findByNameNotInAndGroupId(List<String> names, Long id);
 }
